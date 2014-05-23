9bf8a36a531f6844d21a26f46526d3d59c24b106
Jeremy Singer-Vine <jsvine@gmail.com>
2014-05-23 17:58:00 -0400

    Tweak basic example

--- examples/js/basic.js
+++ examples/js/basic.js
@@ -3,16 +3,30 @@
 
     var p = chartbase.plugins;
 
-    var chart = root.chartbase(".chart")
+    var first_el = d3.select("body").append("svg")
+        .attr("class", "chart")
+        .node();
+
+    var second_el = d3.select("body").append("svg")
+        .attr("class", "chart")
+        .node();
+
+    var first = chartbase(first_el)
         .use(p.resize(200, 500))
-        .use(p.background("blue"))
-        .use(p.resize(500, 200, { delay: 400 }))
-        .use(p.background("red"))
-        .use(p.golden_resize("landscape", { duration: 500 }))
-        .use(p.background("white", {
-            "stroke-width": 1,
-            stroke: "black"
+        .use(p.resize(200, 300))
+        .use(p.background("papayaWhip", {
+            "stroke-width": 2,
+            stroke: "#000"
         }))
         .build();
-    
+
+    var second = chartbase(second_el)
+        .use(p.resize(100, 300))
+        .use(p.background("red"))
+        .use(p.resize(300, 100, { delay: 400 }))
+        .use(p.background("blue"))
+        .use(p.golden_resize("landscape", { duration: 500 }))
+        .use(p.background("red"))
+        .build();
+
 }).call(this);
